9d5012c77e3e3285419f4bbb95cee7af64b69e76
Imannuel <70452074+GtFoBAE05@users.noreply.github.com>
2026-09-17 14:46:00 +0700

    Refactor error handling for context and group session

--- kafka/error.go
+++ kafka/error.go
@@ -16,12 +16,17 @@ func IsTransientError(err error) bool {
 		return false
 	}
 
-	if errors.Is(err, context.DeadlineExceeded) {
+	if errors.Is(err, kgo.ErrClientClosed) {
+		return false
+	}
+
+	var groupSessionErr *kgo.ErrGroupSession
+	if errors.As(err, &groupSessionErr) {
 		return true
 	}
 
-	if errors.Is(err, kgo.ErrClientClosed) {
-		return false
+	if errors.Is(err, context.DeadlineExceeded) {
+		return true
 	}
 
 	return kgo.IsRetryableBrokerErr(err)
